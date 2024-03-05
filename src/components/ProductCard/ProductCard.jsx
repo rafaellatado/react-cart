c86cf2150d337d45278a6966d167d6476bde0d81
Rafael Latado <110048314+rafaellatado@users.noreply.github.com>
2024-03-05 14:32:33 -0300

    Update ProductCard.jsx

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -13,11 +13,26 @@ function ProductCard({ data }) {
 
   const handleAddCart = () => setCartItems([ ...cartItems, data ]);
 
+  /* The spread operator "..." spreads the elements of the cartItems array into a new array. This is important because 
+  directly mutating the state could lead to unexpected behavior in React due to its reliance on immutable data principles. 
+  By creating a new array with the spread syntax, we ensure that we are not mutating the original state directly.
+  The "data" part adds the data object to the end of the new array. Essentially, it appends the data object to the copy 
+  of the cartItems array.
+  In each iteration, the spread operator ensures that we create a new array with the existing items in cartItems, 
+  and then we append the new data object to it. 
+  
+  Also, in React, when you call a state update function like setCartItems, React doesn't immediately update the state. 
+  Instead, it schedules the state update and re-renders the component with the updated state at the end of the current 
+  execution context. It's only when the function completes execution and React reconciles the changes that the state is 
+  actually updated, and the component is re-rendered with the new state.*/
+
   return (
     <section className="product-card">
       
       <img
         src={thumbnail.replace(/\w\.jpg/gi, 'W.jpg')}
+        /* "thumbnail" is a property of the Mercado Livre API, and above we are replacing the picture for a 
+        better quality picture based on the API's specific syntax rules */
         alt="product"
         className="card__image"
       />
