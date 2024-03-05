74a75f5487b6af8b5625c1be246fa7b4cd6b7e8d
Rafael Latado <110048314+rafaellatado@users.noreply.github.com>
2024-03-05 15:24:51 -0300

    Update CartItem.jsx

--- src/components/CartItem/CartItem.jsx
+++ src/components/CartItem/CartItem.jsx
@@ -10,11 +10,16 @@ function CartItem({ data }) {
 
   const { cartItems, setCartItems } = useContext(AppContext);
   const { id, thumbnail, title, price } = data;
+  /* The line above is saying that I'll be able to access the d, thumbnail, title, price properties of 'data'
+  without having to use data.thumbnail, for example. Only 'thumbnail' will already work. */
 
   const handleRemoveItem = () => {
     const updatedItems = cartItems.filter((item) => item.id != id);
     setCartItems(updatedItems);
   };
+  /* The filter() method creates a new array containing elements for which the provided filtering function returns true. 
+  Therefore, items that return true for the condition item.id !== id are kept in the filtered array, 
+  while items that return false are removed. */
 
   return (
     <section className="cart-item">
