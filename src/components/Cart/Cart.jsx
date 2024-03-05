a12466da028ee3396d9bd7e4a9b3b5a00e76649f
Rafael Latado <110048314+rafaellatado@users.noreply.github.com>
2024-03-05 15:09:27 -0300

    Update Cart.jsx

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -9,6 +9,15 @@ function Cart() {
   const { cartItems, isCartVisible } = useContext(AppContext);
 
   const totalPrice = cartItems.reduce((acc, item) => item.price + acc, 0);
+  /* In here we're using the reduce method to calculate the totalPrice in the cartItems' array.
+  The reduce method takes a callback function as its first argument and an initial value 
+  as a second argument (in this case, 0). The callback's first parameter 
+  (commonly named acc, short for "accumulator", but not a reserved word) always represents the accumulated value computed so far 
+  during the reduction process. It starts with the initial value provided to reduce() (which is 0 in this case) and is 
+  updated with each iteration of the method.
+  The second parameter (commonly named item, but also not a reserved word) represents the current element 
+  being processed in the array during each iteration of the reduce() method. 
+  It changes with each iteration, cycling through each element of the array.*/
 
   return (
     <section className={`cart ${isCartVisible ? 'cart--active' : ''}`}>
